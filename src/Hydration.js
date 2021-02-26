e7d4dbf04e3ff57db7de36b862c792b1f3001b17
Jackson McGuire <jaxmcguire@gmail.com>
2021-02-25 21:53:06 -0500

    Get All tests passing

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,8 +4,6 @@ class Hydration extends User {
     super(userData);
     this.date = data.date;
     this.ounces = data.numOunces;
-    // this.ouncesAverage = 0;
-    this.ouncesRecord = [];
     this.drink(userRepository);
   }
   drink(userRepo) {
@@ -17,27 +15,18 @@ class Hydration extends User {
 
   updateHydration(user) {
     const hydrationDataCheck = 
-    user.hydrationData.find(hydrationObj => hydrationObj[this.date])
+    user.hydrationData.find(hydrationObj => hydrationObj[this.date]);
+    
     if (!hydrationDataCheck) {
-      
       user.hydrationData.unshift({[this.date]: this.ounces});
-      // if (user.hydrationData.length()) {
-      // console.log(user.hydrationData)
+      console.log(user.hydrationData)
       const totalOunces = user.hydrationData.reduce((numAcc, hydrationObj) => {
         numAcc += Object.values(hydrationObj)[0];
-        // console.log(hydrationObj)
-        console.log(Object.values(hydrationObj)[0]))
         return numAcc;
       }, 0);
-        // console.log(user.hydrationData, totalOunces)
       user.ouncesAverage = Math.round(totalOunces / user.hydrationData.length);
     }
-    // console.log(user.ouncesAverage)
-    // Math.round((this.ounces + (user.ouncesAverage * 
-    //     (user.hydrationData.length - 1))) / user.hydrationData.length);
-    // } else {
-    // console.log(user.ouncesAverage)
-    // user.ouncesAverage = this.ounces;
+
   }   
   
 
