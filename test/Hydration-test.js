591cc7aa4e63f9f7822260bdb27d5d93f58fcca6
Jackson McGuire <jaxmcguire@gmail.com>
2021-02-25 21:22:28 -0500

    Refactor test and beforeEach hook issue

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -3,79 +3,42 @@ import { expect } from 'chai';
 import Hydration from '../src/Hydration';
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
+import hydrationTestData from './Hydration-test-data'
 
-describe('Hydration', function() {
-  let hydration;
+describe.only('Hydration', function() {
   let user1;
   let user2;
-  let userRepository;
   let hydrate1;
   let hydrate2;
-  let hydrate3;
-
+  let doubleHydrate;
+  let userRepository;
   beforeEach(() => {
-    user1 = new User({
-      'id': 1,
-      'name': 'Luisa Hane',
-      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-      'email': 'Diana.Hayes1@hotmail.com',
-      'strideLength': 4.3,
-      'dailyStepGoal': 10000,
-      'friends': [
-        16,
-        4,
-        8
-      ]
-    })
-    user2 = new User({
-      "id": 2,
-      "name": "Jarvis Considine",
-      "address": "30086 Kathryn Port, Ciceroland NE 07273",
-      "email": "Dimitri.Bechtelar11@gmail.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 5000,
-      "friends": [
-        9,
-        18,
-        24,
-        19
-      ]
-    })
+    user1 = new User(hydrationTestData[0][0]);
+    user2 = new User(hydrationTestData[0][1]);
     userRepository = new UserRepository();
     userRepository.users.push(user1, user2);
-    hydrate1 = new Hydration({
-        "userID": 1,
-        "date": "2019/06/15",
-        "numOunces": 37
-      }, userRepository);
-    hydrate2 = new Hydration({
-      "userID": 2,
-      "date": "2019/06/15",
-      "numOunces": 75
-    }, userRepository)
-    hydrate3 = new Hydration({
-      "userID": 2,
-      "date": "2019/06/16",
-      "numOunces": 91
-    }, userRepository)
+    hydrate1 = new Hydration(user1, hydrationTestData[1][0], userRepository);
+    hydrate2 = new Hydration(user2, hydrationTestData[1][1], userRepository);
+    userRepository.users.push(user1, user2);
+    doubleHydrate = hydrationTestData[1][2];
+    user2.hydrationData.push(doubleHydrate)
   })
-  it('should be a function', function() {
-    expect(Hydration).to.be.a('function');
-  });
   it('should be an instance of hydrate', function() {
     expect(hydrate1).to.be.an.instanceof(Hydration);
   });
   it('should have an id', function() {
-    expect(hydrate2.userId).to.equal(2);
+    expect(hydrate2.id).to.equal(2);
   });
   it('should have a date', function() {
-    expect(hydrate3.date).to.equal('2019/06/16');
+    expect(hydrate2.date).to.equal('2019/06/15');
   });
   it('should have an amount of ounces drank', function() {
-    expect(hydrate3.ounces).to.equal(91);
+    expect(hydrate2.ounces).to.equal(75);
   });
   describe('drink', function () {
-    it('should update the average number of ounces over all time', function() {
+    it.only('should update the average number of ounces over all time', function() {
+      console.log(user2.hydrationData)
+      hydrate2.updateHydration(user2);
       expect(user2.ouncesAverage).to.equal(83);
     })
     it('should add the date and amount to the object record', function() {
